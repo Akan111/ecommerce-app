e56a5b07c5f9a159aefbf60c35b575326ac56ece
Akan Victor <akan380@gmail.com>
2020-10-13 21:40:27 +0100

    react router/custom layout/ footer component

---
+++ src/Layouts/MainLayout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from './../components/header';
+import './MainLayout.scss'
+
+const MainLayout = (props) => {
+    return (
+        <div>
+            <Header />
+            <div className="main">
+                {props.children}
+            </div>
+            <Footer />
+        </div>
+    );
+}
+ 
+export default MainLayout;
